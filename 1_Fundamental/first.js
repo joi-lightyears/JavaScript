30330277e564526a45b063cd28c0dd68acc399dc
Joi Kun <kunthanhdat@gmail.com>
2021-08-30 23:28:02 +0700

    update 🐱‍👤

---
+++ 1_Fundamental/first.js
@@ -0,0 +1,66 @@
+// *********** variable ******************************
+/*
+var myName = 'Joi';
+var age= '18'; // khong co dau cham phay cung duoc
+alert(myName);
+alert(age);
+*/
+
+// *********** comment ******************************
+// hotkey: Ctrl + / => // comment on line
+// hotkey: Shift + Alt + A => Toggle block comment
+
+// ********** built-in function ********************
+/*
+1 so ham built-in (ham co san)
+ 1. Alert
+ 2. Console
+ 3. Confirm
+ 4. Prompt
+ 5.Set timeout
+ 6. Set interval
+---------------------------------------*/
+//2. Console
+//onsole.log('Hello'); //display message
+
+//3. Confirm
+//confirm('18+ ?'); // show Yes/No
+
+//4. Prompt
+//prompt('18+ ?'); //display Input
+
+//5. Set timeout
+// meaning: cho 1 doan code chay sau mot khoang thoi gian cho truoc (don vi time: ms)
+/* setTimeout(function() {
+    alert('notification')
+}, 10000) */ //alert after 10s
+
+//6. Set interval
+/*meaning: cho 1 doan code chay sau mot khoang thoi gian cho truoc nhung
+sau khi chay lan dau no se khong stop (don vi time: ms)*/
+/* setInterval(function() {
+    alert('notification')
+}, 10000); */ // sau khi chay lan dau thi cach 10s no chay 1 lan
+
+// ********** operator ***************************
+/*
+1. So hoc (Arithmetic)
++ - * /  Cong tru nhan chia
+** luy thua
+% chia lay so du
+++ tang 1 gia tri so
+-- tru 1 gia tri so
+
+++ --
+Prefix & Postfix (tien to va hau to)
+prefix: ++a; --a (tra ve luon a da tang hoac giam gia tri)
+profix: a++; a-- (buoc 1 tra ve gia tri nhu cu la a, sau khi do moi tra ve gia tri da tang/giam)
+vd: number = 5
+output = number++ - --number
+meaning: 5 - 5 = 0
+
+2. Toan tu gan (Assignment) a = b (assign b to a)
+3. Toan tu so sanh (Comparision) >, <, =, >=, <=
+4. Toan tu logic (Logical) && ||
+--------------------------------------*/
+
---
+++ README.md
@@ -0,0 +1,1 @@
+[![Javascript Badge](https://img.shields.io/badge/-Javascript-F0DB4F?style=for-the-badge&labelColor=black&logo=javascript&logoColor=F0DB4F)](#)
